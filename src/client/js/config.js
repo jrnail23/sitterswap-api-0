17ed4d172e8c25a6b78c0bee5cef3836ad7e3696
James Nail <james.nail@lanetix.com>
2015-12-23 12:25:13 -0500

    remove client stuff

--- src/client/js/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  serverBaseUrl: process.env.SERVER_BASE_URL || 'http://localhost:9006/'
-}
--- src/client/js/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router } from 'react-router'
-import routes from './routes'
-
-render(<Router routes={routes} />, document.getElementById('app'))
